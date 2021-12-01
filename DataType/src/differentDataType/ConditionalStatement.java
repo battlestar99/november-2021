7a5b6bd7fde01c1d213ab14eaa1b25623a7c8591
“[Razin <anik@agile1tech.com>
2021-11-30 21:17:20 -0500

    Tuesday class upload

---
+++ DataType/src/differentDataType/ConditionalStatement.java
@@ -0,0 +1,56 @@
+package differentDataType;
+
+public class ConditionalStatement {
+
+	public static void main(String[] args) {
+				
+		int value = 1000;
+		
+		if(value >= 90) {
+			System.out.println("Grade is A");
+		}else if (value <= 89 && value >= 80) {
+			System.out.println("Grade is B");
+		}else if (value <= 79 && value >= 70) {
+			System.out.println("Grade is C");
+		}else {
+			System.out.println("Fail");
+		}
+
+		
+
+		/*
+		 * Create a marking system
+		 * you should have a variable for "mark"
+		 * then you have to write a conditional statement for marking system
+		 * 
+		 * for 90 my output should be A
+		 * for 80 my output should be B
+		 * C
+		 * D
+		 * Fail
+		 * 
+		 */
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+//		int myFirstValue = 30;
+//		int mySecondValue = 10;
+//		
+//		if(myFirstValue == mySecondValue) {
+//			System.out.println("My first value is less than second value");
+//		}else if(myFirstValue >= mySecondValue) {
+//			System.out.println("My first value is greater than second value");
+//		}else {
+//			System.out.println("both are equal");
+//		}
+
+	}
+
+}
---
+++ DataType/src/differentDataType/IntData.java
@@ -0,0 +1,55 @@
+package differentDataType;
+
+public class IntData {
+
+	public static void main(String[] args) {
+		/**
+		 * 
+		 * 1. Declare two int data type 
+		 * 2. Initialize with a value
+		 * 3. show the sum of that two int variable
+		 * 4. show the subtraction of that two int variable
+		 * 5. show the multiplication of that two int variable
+		 * 6. show the division of that two int variable
+		 * 
+		 */
+		
+		
+		int firstValue = 30;
+		int secondValue = 10;
+		
+		int sum = firstValue + secondValue;
+
+			
+		String commonString = "of two numbers are: ";
+		
+		System.out.println("Summation "+commonString+ sum);
+		System.out.println("Subtraction of two numbers are: "+(firstValue - secondValue));
+		System.out.println("Multiplication of two numbers are: "+firstValue * secondValue);
+		System.out.println("Division os two numbers are: "+firstValue / secondValue);
+		
+		System.out.println("Summation of two numbers are: "+ sum);
+		System.out.println("Subtraction of two numbers are: "+(firstValue - secondValue));
+		System.out.println("Multiplication of two numbers are: "+firstValue * secondValue);
+		System.out.println("Division of two numbers are: "+firstValue / secondValue);
+		
+		System.out.println("Summation of two numbers are: "+ sum);
+		System.out.println("Subtraction of two numbers are: "+(firstValue - secondValue));
+		System.out.println("Multiplication of two numbers are: "+firstValue * secondValue);
+		System.out.println("Division of two numbers are: "+firstValue / secondValue);
+		
+		System.out.println("Summation of two numbers are: "+ sum);
+		System.out.println("Subtraction of two numbers are: "+(firstValue - secondValue));
+		System.out.println("Multiplication of two numbers are: "+firstValue * secondValue);
+		System.out.println("Division of two numbers are: "+firstValue / secondValue);
+		
+		System.out.println("Summation of two numbers are: "+ sum);
+		System.out.println("Subtraction of two numbers are: "+(firstValue - secondValue));
+		System.out.println("Multiplication of two numbers are: "+firstValue * secondValue);
+		System.out.println("Division of two numbers are: "+firstValue / secondValue);
+		
+		
+		System.out.println("My "+"name "+"is "+"john");
+	}
+
+}
